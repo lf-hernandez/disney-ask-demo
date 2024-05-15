8b8d74950e1f413e5daece98790b19c0739cf60e
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-05-15 11:42:38 -0400

    Integrate dynamodb logic

--- index.ts
+++ index.ts
@@ -1,22 +1,35 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
 import { APIGatewayEvent, Context, Handler } from 'aws-lambda';
-import { DynamoDB } from 'aws-sdk';
 
-// source: https://pizzabottle.com/85902-disney-facts-blow-mind/
-const movieFacts = [
-	"The vultures in The Jungle Book were supposed to be voiced by The Beatles; however, due to a scheduling conflict they couldn’t make it to the studio so they had other actors instead. Hence why they look like The Beatles in the film.",
-	"101 Dalmatians, Peter Pan, Lady and the Tramp, and Mulan are the only animated Disney films where both parents are present and don’t die during the movie.",
-	"Bambi only uses 72 words throughout the entire film."
-];
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler: Handler = async (event: APIGatewayEvent, context: Context) => {
-	console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-	console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
-	const randomIndex = Math.floor(Math.random() * movieFacts.length);
-	const randomFact = movieFacts[randomIndex];
+    const command = new ScanCommand({
+        ProjectionExpression: '#N, #F',
+        ExpressionAttributeNames: { '#N': 'movie_name', '#F': 'movie_fact' },
+        TableName: 'movie_facts',
+    });
 
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ trivia: randomFact }),
-	}
-}
+    try {
+        const response = await docClient.send(command);
+        if (response.Items) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response.Items),
+            };
+        }
+
+        return {
+            statusCode: 200,
+            body: 'empty dataset',
+        };
+    } catch (error) {
+        console.log(`operation failed\nerror:${error}`);
+        return { statusCode: 500, body: 'failed to return facts' };
+    }
+};
