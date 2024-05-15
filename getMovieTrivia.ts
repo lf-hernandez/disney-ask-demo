150d6bf2a3ba1d78bec4309de4fe1d542b72ccc9
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-05-15 01:13:46 -0400

    Add movie facts lambda function

---
+++ getMovieTrivia.ts
@@ -0,0 +1,18 @@
+import { Handler } from 'aws-lambda';
+
+// source: https://pizzabottle.com/85902-disney-facts-blow-mind/
+const movieFacts = [
+	"The vultures in The Jungle Book were supposed to be voiced by The Beatles; however, due to a scheduling conflict they couldn’t make it to the studio so they had other actors instead. Hence why they look like The Beatles in the film.",
+	"101 Dalmatians, Peter Pan, Lady and the Tramp, and Mulan are the only animated Disney films where both parents are present and don’t die during the movie.",
+	"Bambi only uses 72 words throughout the entire film."
+];
+
+export const handler: Handler = async () => {
+	const randomIndex = Math.floor(Math.random() * movieFacts.length);
+	const randomFact = movieFacts[randomIndex];
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ trivia: randomFact }),
+	}
+}
