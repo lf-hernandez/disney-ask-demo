9c5ce9e57496b95409a182d66608d5ab56698d79
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-05-15 21:41:41 -0400

    Add error handling to update script

--- update.sh
+++ update.sh
@@ -1,1 +1,17 @@
-aws lambda update-function-code --function-name $FUNC_NAME --zip-file fileb://dist/index.zip
+#!/bin/bash
+
+npm run build
+if [ $? -eq 0 ]; then
+    echo "build successful. Updating Lambda function..."
+    
+    aws lambda update-function-code --function-name ${FUNCTION_NAME} --zip-file fileb://dist/index.zip
+    if [ $? -eq 0 ]; then
+        echo "Lambda function updated successfully."
+    else
+        echo "error: Failed to update Lambda function."
+        exit 1
+    fi
+else
+    echo "error: Build failed."
+    exit 1
+fi
